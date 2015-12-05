e428ffe54c50c34f6fe66411ce8031e8687a8e46
Mike Nikles <mike@vanniksoftware.com>
2015-12-05 11:33:38 -0800

    Added the 'set' command

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var Parse = require('parse/node');
 var config = require('./config.json');
+var configKeyWhitelist = ['minRating', 'maxRating'];
 
 var loadReviewFromItunes = function(appIdList, successCallback, errorCallback) {
   var randomAppIndex = Math.floor(Math.random() * (appIdList.length));
@@ -106,6 +107,51 @@ var listTeamSettings = function(teamDomain, successCallback, errorCallback) {
   });
 };
 
+var setTeamSettingsConfigValue = function(teamDomain, configKey, configValue, successCallback, errorCallback) {
+  var TeamSettings = Parse.Object.extend('TeamSettings');
+  var query = new Parse.Query(TeamSettings);
+  query.equalTo('teamDomain', teamDomain);
+  query.find({
+    success: function(results) {
+      // No settings yet, create a new entry
+      if (results.length === 0) {
+        var newTeamSettings = new TeamSettings();
+        newTeamSettings.set('teamDomain', teamDomain);
+        newTeamSettings.set('appIdList', []);
+        if (_.contains(configKeyWhitelist, configKey)) {
+          newTeamSettings.set(configKey, configValue);
+        }
+        newTeamSettings.save(null, {
+          success: function(persistedTeamSettings) {
+            successCallback('Success! Your settings are persisted.');
+          },
+          error: function(persistedTeamSettings, error) {
+            // Execute any logic that should take place if the save fails.
+            // error is a Parse.Error with an error code and message.
+            console.log('Failed to create new object, with error code: ' + error.message);
+            errorCallback('Failed to create new object, with error code: ' + error.message);
+          }
+        });
+      // Settings available, update
+      } else if (results.length === 1) {
+        if (_.contains(configKeyWhitelist, configKey)) {
+          results[0].set(configKey, configValue);
+        }
+        results[0].save();
+        successCallback('Success! Your settings are persisted.');
+      // More than 1 settings entry for the given team... problem
+      } else {
+        console.log('More than 1 settings entry for team: ' + teamDomain);
+        errorCallback('Something went wrong... Please try again');
+      }
+    },
+    error: function(error) {
+      console.log('Cannot load team settings for team: ' + teamDomain + '. Error: ' + error);
+      errorCallback('Something went wrong... Please try again');
+    }
+  });
+};
+
 var processCommand = function(teamDomain, parameters, successCallback, errorCallback) {
   var parametersArray = parameters.split(' ');
   var command = parametersArray[0];
@@ -114,6 +160,10 @@ var processCommand = function(teamDomain, parameters, successCallback, errorCall
     persistTeamSettings(teamDomain, appIdList, successCallback, errorCallback);
   } else if (command === 'list') {
     listTeamSettings(teamDomain, successCallback, errorCallback);
+  } else if (command === 'set') {
+    var configKey = parametersArray[1].split('=')[0];
+    var configValue = parametersArray[1].split('=')[1];
+    setTeamSettingsConfigValue(teamDomain, configKey, configValue, successCallback, errorCallback);
   }
   // TODO: Add more commands: 'reset', 'remove'
 };
