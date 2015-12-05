f113a7520e1b964b1393b49f0b97183a574f454a
Mike Nikles <mike@vanniksoftware.com>
2015-12-05 12:13:59 -0800

    Minor styling update for the help command

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ var processCommand = function(slackCommand, teamDomain, parameters, successCallb
       '    `add`, `list`, `set`, `help`\n\n' +
       '`' + slackCommand + ' add <list of app IDs>    ` A comma separated list of app IDs from iTunes Connect\n' +
       '`' + slackCommand + ' list                     ` Lists all configured app IDs\n' +
-      '`' + slackCommand + ' set <configKey>=<value>  ` where <configKey> is `minRating` or `maxRating`. A value between 1 and 5, inclusive\n' +
+      '`' + slackCommand + ' set <configKey>=<value>  ` where `<configKey>` is `minRating` or `maxRating`. A value between 1 and 5, inclusive\n' +
       '`' + slackCommand + ' help                     ` Help\n';
     successCallback(helpMessage);
   }
