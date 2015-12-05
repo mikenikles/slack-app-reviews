0a12563bec0cf6c1ee9f284d1e3ba0c97a8e675f
Mike Nikles <mike@vanniksoftware.com>
2015-12-05 12:40:39 -0800

    Updated help

--- index.js
+++ index.js
@@ -188,9 +188,10 @@ var processCommand = function(slackCommand, teamDomain, parameters, successCallb
     setTeamSettingsConfigValue(teamDomain, configKey, configValue, successCallback, errorCallback);
   } else if (command === 'help') {
     var helpMessage = 'Welcome to `slack-app-reviews`!\n\n' +
-      'Usage: `' + slackCommand + ' <command>`\n\n' +
+      'Usage: `' + slackCommand + ' [<command>]`\n\n' +
       'where `<command>` is one of:\n' +
       '    `add`, `list`, `set`, `help`\n\n' +
+      '`' + slackCommand + '                          ` Displays a random review\n' +
       '`' + slackCommand + ' add <list of app IDs>    ` A comma separated list of app IDs from iTunes Connect\n' +
       '`' + slackCommand + ' list                     ` Lists all configured app IDs\n' +
       '`' + slackCommand + ' set <configKey>=<value>  ` where `<configKey>` is `minRating` or `maxRating`. A value between 1 and 5, inclusive\n' +
